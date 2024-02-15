ee190d242318dd6100af165cb11cdeacfa31d601
alanjohnck <alanjohnchacko.live@gmail.com>
2024-02-16 01:07:54 +0530

    project-setup

---
+++ src/app/meetingpage/page.tsx
@@ -0,0 +1,11 @@
+
+export default function MeetingPage(){
+    return (
+
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1>
+                Meeting Page
+            </h1>
+        </div>
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div className="flex flex-col items-center justify-center h-screen">
+    <h1>
+      Landing Page of "TRANSCEND"
+    </h1>
+  </div>
+  );
+}
