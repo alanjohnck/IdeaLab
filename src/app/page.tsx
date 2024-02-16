707daa919ce1b6e9aaab9419457f2002adf50ef3
alanjohnck <alanjohnchacko.live@gmail.com>
2024-02-16 15:13:56 +0530

    changed buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,24 +6,26 @@ export default function Home() {
  {/**frame 1 */}
    <div className="flex flex-col h-screen gap-3 bg-white">
    {/**NAVBAR */}
-    <div className="flex flex-row w-screen h-24 bg-blue-100 items-center justify-start">
+    <div className="flex flex-row w-screen h-24 bg-white items-center justify-start">
       <img className="flex w-43 h-32 p-6" src="Component 2.svg"></img>    
     </div>
     {/**NAVBAR */}
-   
-    <div className="flex flex-row w-screen h-full items-center justify-start gap-2 p-6 ">
-      <div className="flex flex-col w-1/2 h-2/3 items-center justify-center border border-blue-100 rounded-md">
-        <div className="flex flex-col items-start justify-start p-5">
-           <h1 className="flex justify-start  text-blue-300  text-2xl font-extrabold">Breaking Language Barriers with Real-Time Translation</h1>
+    <div className="border border-[#98C3D1] w-[250px] h-[250px] rounded-md self-start  z-10 absolute top-40 left-20"></div>
+
+    <div className="flex flex-row w-screen h-full items-center justify-start gap-10 p-10 absolute">
+      <div className="flex flex-col w-1/2 h-1/2  items-center justify-center border border-blue-100 rounded-md gap-6">
+        <div className="flex flex-col items-start justify-start p-10 gap-6">
+           <h1 className="flex justify-start  text-blue-300  text-3xl font-bold">Breaking Language Barriers with Real-Time Translation</h1>
            <br></br>
-           <p className="text-black text-lg font-regular">Conversations across languages often hit roadblocks, leading to misunderstandings and hindering effective communication. At TRANSCEND, we believe in making language no longer a barrier in conversations. Introducing our groundbreaking solution: a comprehensive real-time language translation software.</p>
+           <p className="text-black text-lg font-light leading-10">Conversations across languages often hit roadblocks, leading to misunderstandings and hindering effective communication. At TRANSCEND, we believe in making language no longer a barrier in conversations. Introducing our groundbreaking solution: a comprehensive real-time language translation software.</p>
         </div>
       </div>
       <div className="flex w-1/2 h-2/3 items-center justify-center">
           <img src="destop-removebg-preview 1.svg"></img>
       </div>
+     
     </div>
-    
+   
    </div>
   {/**frame 1 */}
 
