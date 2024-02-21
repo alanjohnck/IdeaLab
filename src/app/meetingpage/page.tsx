7a581b4c135716951cb5c3561bb731e5b2afef35
alanjohnck <alanjohnchacko.live@gmail.com>
2024-02-21 12:02:52 +0530

    Added meeting interface

--- src/app/meetingpage/page.tsx
+++ src/app/meetingpage/page.tsx
@@ -1,12 +1,34 @@
 "use client"
 
+import axios from "axios";
+
+import React,{useState,useCallback} from "react"
+
+import { useRouter } from "next/navigation";
 
 
-const MeetingPage = () => {
+
+export default function MeetingPage(){
+
+
+const router = useRouter();
+const [value,setValue]=useState("");
+const handleJoinRoom =useCallback(()=>{
+ router.push(`/room/${value}`);
+},[router,value])
+
  return (
- <div>
-     <h1>Meeting page</h1>
+ <div className="flex flex-col h-screen w-screen items-center justify-center gap-5">
+     
+     <h1>Join room</h1>
+     <input 
+     className="text-black "
+     type="text"
+      value={value} 
+      onChange={(e)=>setValue(e.target.value)} placeholder="enter the room code">
+
+     </input>
+     <button className="bg-[#2096BD] w-[100px] h-[40px] rounded-lg " onClick={handleJoinRoom}>join</button>
  </div>
  );
 }
-export default MeetingPage;
